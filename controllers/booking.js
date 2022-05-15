daea3c8949cea3162d4aea84661d2343bf0e1856
aa-nikitin <a.nikitin91@mail.ru>
2022-05-16 00:15:45 +0300

    change paymentSum for payment.js

--- controllers/booking.js
+++ controllers/booking.js
@@ -378,7 +378,7 @@ module.exports.bookingNotice = async (req, res) => {
         const timeFormat = moment(plan.time).format(formatTimeConf);
         const timeToFormat = moment(plan.time).add(plan.minutes, 'm').format(formatTimeConf);
         const priceCalc = plan.price - plan.discount;
-        // console.log('1 - ', req.body);
+        console.log('1 - ', req.body);
         const priceCalcPercent = parseInt(Math.ceil((priceCalc / 100) * percent));
         console.log('2 - ', priceCalcPercent, priceCalc, percent);
         const textPlan = `<div><a href="${hostname}/detail-plan/${plan.id}">${plan.hall.name} (${dateFormat} ${timeFormat} - ${timeToFormat})</a></div>`;
